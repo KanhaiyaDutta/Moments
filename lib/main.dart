e2e9397af7d6066551978a74b3d5cec5b5ccaba0
KanhaiyaDutta <duttakanhaiya@gmail.com>
2023-08-25 01:38:06 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(VideoCallApp());
+}
+
+class VideoCallApp extends StatefulWidget {
+  const VideoCallApp({super.key});
+
+  @override
+  State<VideoCallApp> createState() => _VideoCallAppState();
+}
+
+class _VideoCallAppState extends State<VideoCallApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
---
+++ lib/screens/call_screen.dart
@@ -0,0 +1,1 @@
+import 'package:socket_io_client/socket_io_client.dart';
