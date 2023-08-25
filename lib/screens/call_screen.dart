e40a656cb9a4bedb57c0abc0b2dfbad2f2a7fd29
KanhaiyaDutta <duttakanhaiya@gmail.com>
2023-08-25 19:00:25 +0530

    join_screen

--- lib/screens/call_screen.dart
+++ lib/screens/call_screen.dart
@@ -1,1 +1,22 @@
-import 'package:socket_io_client/socket_io_client.dart';
+import 'package:flutter/material.dart';
+
+class CallScreen extends StatefulWidget {
+  final String callerId, calleeId;
+  final dynamic offer;
+  const CallScreen({
+    super.key,
+    required this.callerId,
+    required this.calleeId,
+    this.offer,
+  });
+
+  @override
+  State<CallScreen> createState() => _ClassScreenState();
+}
+
+class _ClassScreenState extends State<CallScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
